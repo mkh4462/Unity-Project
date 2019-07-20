24567829a6ab1f9f66846d59e1d6a27bbf805169
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2019-07-20 08:47:45 +0530

    uploading

---
+++ Runner_in_Space/Assets/Script/ReapitingBG2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReapitingBG2 : MonoBehaviour
+{
+    public float _speed;
+
+    public float endy;
+    public float starty;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * _speed * Time.deltaTime);
+
+        if (transform.position.x <= endy)
+        {
+            Vector2 pos = new Vector2(starty, transform.position.y);
+            transform.position = pos;
+        }
+    }
+}
---
+++ Runner_in_Space/Assets/Script/Spwaner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spwaner : MonoBehaviour
+{
+    public GameObject[] ObstaclePatterns;
+
+    private float timeBtwSpawn;
+    public float startTimebtwSpawn;
+
+    public float decreaseTime;
+    public float minTime = 0.65f;
+
+    private void Update()
+    {
+        if(timeBtwSpawn <=0)
+        {
+            int rand = Random.Range(0, ObstaclePatterns.Length);
+            Instantiate(ObstaclePatterns[rand], transform.position, Quaternion.identity);
+            timeBtwSpawn = startTimebtwSpawn;
+            if(startTimebtwSpawn > minTime){
+                startTimebtwSpawn -= decreaseTime;
+
+            }
+        }
+        else
+        {
+            timeBtwSpawn -= Time.deltaTime;
+        }
+    }
+}
